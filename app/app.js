1011448cdc1ffc71f70069cd53d5bed90dbf04ee
Franco Ottaviani <ottavianifrancom@gmail.com>
2022-09-27 01:51:03 -0300

    a little bit of CSS

--- app/app.js
+++ app/app.js
@@ -6,13 +6,22 @@ let threePoint = document.querySelector("#threePoint");
 let fourPoint = document.querySelector("#fourPoint");
 let fivePoint = document.querySelector("#fivePoint");
 let card = document.querySelector(".card")
-
-
+let boton = document.querySelector(".boton")
+let container = document.querySelector(".container")
+let containerThankYou = document.querySelector(".containerThankYou")
+let choise = document.querySelector(".choise")
 let selectores = [onePoint,twoPoint,threePoint,fourPoint,fivePoint]
 
 // VARIABLES
 
 let seleccionado = null;
+
+
+boton.addEventListener("click", function(){
+    card.style = "display: none";
+    containerThankYou.style = "display: flex";
+    choise.innerHTML = "--> " + seleccionado + " <--";
+})
 
 
 // Recorro el array "selectores"
--- app/thankYou.js
+++
@@ -1,3 +0,0 @@
-let parrafo = document.querySelector("#parrafo")
-
-
